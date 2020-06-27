551d462b14ee5b2d5973803da9062c93786016b2
Michael Navarro <navarro.michael93@gmail.com>
2020-06-27 13:04:53 -0500

    Implement copy animation

--- src/PaletteCard.js
+++ src/PaletteCard.js
@@ -3,15 +3,37 @@ import "./PaletteCard.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class PaletteCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
+
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
+
     return (
       <div style={{ background }} className="PaletteCard">
+        <div
+          style={{ background }}
+          className={`copy-overlay ${copied && "show"}`}
+        />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>copied!</h1>
+          <p>{this.props.background}</p>
+        </div>
         <div className="copy-container">
           <div className="palette-content">
             <span>{name}</span>
           </div>
-          <CopyToClipboard text={background}>
+          <CopyToClipboard text={background} onCopy={this.changeCopyState}>
             <button className="copy-button">Copy</button>
           </CopyToClipboard>
         </div>
