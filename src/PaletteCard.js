2a431813b6bf644b30da3e1a96e31998c6e970d5
Michael Navarro <navarro.michael93@gmail.com>
2020-06-27 12:18:34 -0500

    Style PaletteCard component

--- src/PaletteCard.js
+++ src/PaletteCard.js
@@ -3,13 +3,16 @@ import "./PaletteCard.css";
 
 class PaletteCard extends Component {
   render() {
+    const { name, background } = this.props;
     return (
-      <div
-        style={{ background: this.props.background }}
-        className="PaletteCard"
-      >
-        <span>{this.props.name}</span>
-        <span>More</span>
+      <div style={{ background }} className="PaletteCard">
+        <div className="copy-container">
+          <div className="palette-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
