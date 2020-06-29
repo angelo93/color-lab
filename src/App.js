81263e15e3c374b39a1053cb504da29c0e34e0be
Michael Navarro <navarro.michael93@gmail.com>
2020-06-29 13:06:01 -0500

    Install React Router package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedPalettes[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" />
+      <Route exact path="/palette/:id" />
+    </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedPalettes[4])} />
+    // </div>
   );
 }
 
