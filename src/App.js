d8717d90861d463f6ef0eb7bd89e7ab28a83cc7b
Michael Navarro <navarro.michael93@gmail.com>
2020-06-29 15:17:59 -0500

    Rename PalletList -> PaletteList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
-import PaletteList from "./PalletList";
+import PaletteList from "./PaletteList";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
 
