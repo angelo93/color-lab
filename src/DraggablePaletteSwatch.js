46ff8d4af135b2f1d94e63a0ea1cf06c9fe8f83b
Michael Navarro <navarro.michael93@gmail.com>
2020-06-30 15:07:46 -0500

    Add functionaless Draggable Swatch

---
+++ src/DraggablePaletteSwatch.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  root: {
+    width: "20%",
+    height: "25%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-4px",
+  },
+};
+
+function DraggablePaletteSwatch(props) {
+  return (
+    <div
+      className={props.classes.root}
+      style={{ backgroundColor: props.color }}
+    >
+      {props.color}
+    </div>
+  );
+}
+
+export default withStyles(styles)(DraggablePaletteSwatch);
