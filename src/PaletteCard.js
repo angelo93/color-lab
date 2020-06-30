942b314ea5d9256b5ec002dbe8a281953bafd1ec
Michael Navarro <navarro.michael93@gmail.com>
2020-06-30 11:32:34 -0500

    Finish PaletteCard styling refactor

--- src/PaletteCard.js
+++ src/PaletteCard.js
@@ -18,13 +18,24 @@ const styles = {
       opacity: "1",
     },
   },
+  paletteContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+  },
   copyText: {
     color: (props) =>
       chroma(props.background).luminance() >= 0.7 ? "black" : "white",
   },
   colorName: {
     color: (props) =>
-      chroma(props.background).luminance() <= 0.8 ? "white" : "black",
+      chroma(props.background).luminance() <= 0.6 ? "white" : "black",
   },
   seeMore: {
     color: (props) =>
@@ -63,6 +74,56 @@ const styles = {
     cursor: "pointer",
     opacity: "0",
   },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out",
+    transform: "scale(0.1)",
+  },
+  showOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "10",
+    position: "absolute",
+  },
+  copyMsg: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "aliceblue",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.3)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase",
+    },
+    "& p": {
+      fontSize: "2.5rem",
+      fontWeight: "100",
+    },
+  },
+  showMsg: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.285s",
+  },
 };
 
 class PaletteCard extends Component {
@@ -93,14 +154,14 @@ class PaletteCard extends Component {
       <div style={{ background }} className={classes.PaletteCard}>
         <div
           style={{ background }}
-          className={`copy-overlay ${copied && "show"}`}
+          className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
         />
-        <div className={`copy-msg ${copied && "show"}`}>
+        <div className={`${classes.copyMsg} ${copied && classes.showMsg}`}>
           <h1>copied!</h1>
           <p className={classes.copyText}>{this.props.background}</p>
         </div>
-        <div className="copy-container">
-          <div className="palette-content">
+        <div>
+          <div className={classes.paletteContent}>
             <span className={classes.colorName}>{name}</span>
           </div>
           <CopyToClipboard text={background} onCopy={this.changeCopyState}>
