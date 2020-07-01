a995dc82cc73f3497836468cc60dca70131897ae
Michael Navarro <navarro.michael93@gmail.com>
2020-07-01 09:59:57 -0500

    Setup emoji-mart implementation

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -7,6 +7,9 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { Picker } from "emoji-mart";
+
+import "emoji-mart/css/emoji-mart.css";
 
 export class PaletteMetaForm extends Component {
   constructor(props) {
@@ -56,6 +59,7 @@ export class PaletteMetaForm extends Component {
             <DialogContentText>
               Please name your new palette. Make sure it is a unique name!
             </DialogContentText>
+            <Picker />
             <TextValidator
               label="Palette Name"
               value={newPaletteName}
